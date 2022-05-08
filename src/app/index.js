acab444c27dc641a9ef9f97cc9633b0d3e6e4a38
RyanWatts1 <devshop202@yahoo.com>
2022-05-07 21:02:02 -0400

    added login ui

--- src/app/index.js
+++ src/app/index.js
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
 import { Canvas } from "./components"
+import Login from "../login";
 
 function App() {
   return(
     <Canvas>
       <Router>
         <Routes>
-          
+          <Route path="/" element={<Login />} />
         </Routes>
       </Router>
     </Canvas>
