5b88f6a9554d1dc29be50aa0d44f51944e7357d6
RyanWatts1 <devshop202@yahoo.com>
2022-05-07 20:50:59 -0400

    added canvas

--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,16 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
+import { Canvas } from "./components"
 
 function App() {
-  return (
-    <div className="App">
- hello
-    </div>
-  );
+  return(
+    <Canvas>
+      <Router>
+        <Routes>
+          
+        </Routes>
+      </Router>
+    </Canvas>
+  )
 }
 
 export default App;
