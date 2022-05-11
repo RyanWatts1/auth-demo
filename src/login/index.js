683b2b75c3f3003e30bf8cada91784a801f75083
RyanWatts1 <devshop202@yahoo.com>
2022-05-11 19:48:43 -0400

    added state to login page

--- src/login/index.js
+++ src/login/index.js
@@ -7,15 +7,46 @@ import {
   TextField,
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function Login() {
   const navigate = useNavigate();
+  const [form, setForm] = useState({ email: "", password: "" });
+
+  const handleChange = (event) => {
+    const { value, name } = event.target;
+    setForm((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const login = () => {
+    console.log(form);
+
+    setForm({
+      email: "",
+      password: "",
+    });
+  };
+
   return (
     <Card>
       <Typography variant="h5">Sign in</Typography>
-      <TextField label="Email" type="email" />
-      <TextField label="Password" type="password" />
-      <Button variant="contained">Login</Button>
+      <TextField
+        label="Email"
+        type="email"
+        name="email"
+        value={form.email}
+        onChange={handleChange}
+      />
+      <TextField
+        label="Password"
+        type="password"
+        name="password"
+        value={form.password}
+        onChange={handleChange}
+      />
+      <Button onClick={login} variant="contained">
+        Login
+      </Button>
       <Typography align="center">
         Don't have an account?{" "}
         <Link onClick={() => navigate("/register")}>Register </Link>{" "}
